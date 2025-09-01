905a5ea9bcac7c7127e863acefc04c8785efc5ba
Gergely Balint <gergely.balint@arm.com>
2025-09-01 08:52:28 +0000

    [BOLT][BTI] Add MCPlusBuilder::updateBTIVariant
    Checks if an instruction is BTI, and updates the immediate value to the
    newly requested variant.

--- bolt/lib/Target/AArch64/AArch64MCPlusBuilder.cpp
+++ bolt/lib/Target/AArch64/AArch64MCPlusBuilder.cpp
@@ -2772,6 +2772,14 @@ public:
     Inst.addOperand(MCOperand::createImm(HintNum));
   }
 
+  void updateBTIVariant(MCInst &Inst, bool CallTarget,
+                        bool JumpTarget) const override {
+    assert(Inst.getOpcode() == AArch64::HINT && "Not a BTI instruction.");
+    unsigned HintNum = getBTIHintNum(CallTarget, JumpTarget);
+    Inst.clear();
+    Inst.addOperand(MCOperand::createImm(HintNum));
+  }
+
   InstructionListType materializeAddress(const MCSymbol *Target, MCContext *Ctx,
                                          MCPhysReg RegName,
                                          int64_t Addend = 0) const override {
